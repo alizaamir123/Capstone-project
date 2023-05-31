4910fd626516feddf3b56c276cd7acfbbbbe2fef
aliza amir <Alizaamir49@gmail.com>
2023-05-31 05:17:27 -0700

    add js files

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 import data from './brandsdata.js';
+
+document.querySelector('.brands-founder').innerHTML=data.map(
+    (data)=>` <div class="brands-name flex">
+    <div class="brands-img ">
+        <img src='${data.image}' alt="" class="owner-img">
+    </div>
+    <div class="owner-data">
+        <h3 class="name">${data.name}</h3>
+        <p class="product-desc">${data.about}</p>
+    </div>
+</div>`,
+)
+.join('');
